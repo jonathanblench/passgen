3284b4f9750e00fb6ce8725ff3b556be7cd06d67
jonathanblench <jonathan@blench.me.uk>
2022-06-19 19:30:00 +0100

    Updated file location

--- passgen.py
+++ passgen.py
@@ -2,24 +2,31 @@
 # Script to generate a 3 word password and drop it into the clipboard to use
 # Set path the word file as variable
 wordfilepath = "britcaps.txt"
-# Import the random module
+# Import the required modules
 import random
-# Function to use clipboard
 import os
+import sys
+# Function to use clipboard
 def addToClipBoard(text):
     command = 'echo ' + text.strip()
     os.system(command)
 # Open and read the words file and split words
-fileread1 = open(wordfilepath, "r")
+fileread1 = open(os.path.join(sys.path[0], wordfilepath), "r")
 allcontent = (fileread1.read())
 words = list(map(str, allcontent.split()))
 # Generate 3 random words and store as variables
 word1 = (random.choice(words))
 word2 = (random.choice(words))
 word3 = (random.choice(words))
+#remove apostrophes from words
+word1.replace("'","")
+word2.replace("'","")
+word3.replace("'","")
+# Generate random 4 digit number
+number = random.randint(1111,9999)
+#convert number to string
+convertednum = str(number)
 # Bring words together into string and add dashes between words
-password = word1+"-"+word2+"-"+word3
-# Place password into clipboard and display
-addToClipBoard(password)
+password = word1+"-"+word2+"-"+word3+"-"+convertednum
 # Remove hash from next line to print the password.
 print (password)
